08322d2bc4a91565a96261f7ce9f43c7bf4f489f
Cyrus Vilar <cvilar83@gmail.com>
2024-04-29 19:15:52 +0800

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite coffee here</h2>
+        <p>Choose from a diverse menu featuring different types of great coffee at an affordable price.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
